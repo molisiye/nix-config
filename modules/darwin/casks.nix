c91595cecb0015fdef082127f14a012072635c11
molisiye <molisiye@live.com>
2024-12-11 14:09:00 +0800

    init

---
+++ modules/darwin/casks.nix
@@ -0,0 +1,20 @@
+_:
+
+[
+  # Development Tools
+  "homebrew/cask/docker"
+  "visual-studio-code"
+
+  # Communication Tools
+  "telegram"
+
+  # Utility Tools
+  "syncthing"
+
+  # Entertainment Tools
+
+  # Productivity Tools
+  "raycast"
+
+  # Browsers
+]
